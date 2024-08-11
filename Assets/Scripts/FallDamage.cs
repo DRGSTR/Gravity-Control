e694389c831cdddd9b7308e0904e514ff44f87b5
Dhawal <dhawal_jhaveri@yahoo.com>
2024-08-11 10:47:54 +0530

    updated fall damage

--- Assets/Scripts/FallDamage.cs
+++ Assets/Scripts/FallDamage.cs
@@ -5,51 +5,25 @@ using UnityEngine;
 public class FallDamage : MonoBehaviour
 {
     public GameObject gameOverPanel;
+    private Animator anim;
 
-    private bool wasFalling;
-    private bool wasGrounded;
-    private float startOfFall;
-    private bool _grounded;
-    private Rigidbody rb;
-    private float minimumFall = 10f;
-
-    private void Update()
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>(); 
+        anim = GetComponent<Animator>();    
     }
 
-    private void FixedUpdate()
+    private void OnTriggerEnter(Collider target)
     {
-        CheckGround();
-
-        if (!wasGrounded && _grounded) GameOver();
-        if (!wasFalling && isFalling) startOfFall = transform.position.y;
-
-        wasGrounded = _grounded;
-        wasFalling = isFalling;
-    }
-
-    void CheckGround()
-    {
-        _grounded = Physics.Raycast(transform.position + Vector3.up, -Vector3.up, 1.01f);
-    }
-
-    bool isFalling { get { return (!_grounded && rb.velocity.y < 0f); } }
-
-    void GameOver()
-    {
-        float fallDistance = startOfFall - transform.position.y;
-
-        if (fallDistance > minimumFall)
+        if(target.gameObject.tag == "Respawn")
         {
+            anim.SetBool("IsFalling", true);
             StartCoroutine("Reset");
-            Debug.Log(fallDistance);
         }
     }
 
     IEnumerator Reset()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2f);
         gameOverPanel.SetActive(true);
         Time.timeScale = 0f;
         Debug.Log("Game Over!");
